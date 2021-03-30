adf407111309fa1f59fe3c86229afc87affd1cbe
Purnawirman <purnawirman@gmail.com>
2021-03-30 16:20:03 +0700

    Update MaxCounters.java

--- 4. counting elements/MaxCounters.java
+++ 4. counting elements/MaxCounters.java
@@ -9,15 +9,17 @@ class Solution {
         // write your code in Java SE 8
         int[] result = new int[N];
         int max = 0;
+        int baseline = 0;
         for (int i = 0; i < A.length; i++) {
             if (A[i] > N) {
-                Arrays.fill(result, max);
+                baseline = max;
             } else {
-                result[A[i] - 1] = result[A[i] - 1] + 1;
+                result[A[i] - 1] = Math.max(baseline, result[A[i] - 1]) + 1;
                 max = Math.max(max, result[A[i] - 1]);
             }
         }
-
+        for (int i = 0; i < result.length; i++) 
+            result[i] = Math.max(result[i], baseline);
         return result;
     }
 }
