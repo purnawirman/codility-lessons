5cc63edd4f9e41d2b6b9d1522ba1deb9735d23c1
Purnawirman <purnawirman@gmail.com>
2021-04-02 21:08:39 +0700

    Update ChocolatesByNumbers.java

--- 12. Euclidean Algorithm/ChocolatesByNumbers.java
+++ 12. Euclidean Algorithm/ChocolatesByNumbers.java
@@ -7,7 +7,7 @@
 class Solution {
     public int solution(int N, int M) {
         // write your code in Java SE 8
-        return Math.max(N, M) / gcd(N, M, 1);
+        return N / gcd(N, M, 1);
     }
 
     int gcd(int a, int b, int res) {
