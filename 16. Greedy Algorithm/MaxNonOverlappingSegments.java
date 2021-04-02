be12ff3bbab50d48e4601ba0ec719c12f7dcd49a
Purnawirman <purnawirman@gmail.com>
2021-04-03 01:42:37 +0700

    Create MaxNonOverlappingSegments.java

---
+++ 16. Greedy Algorithm/MaxNonOverlappingSegments.java
@@ -0,0 +1,17 @@
+// you can also use imports, for example:
+// import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        // write your code in Java SE 8
+        if (A.length <= 1) return A.length;
+        int result = 0;
+        for (int i = 1; i < A.length; i++) {
+            if (A[i] > B[i-1]) result++;
+        }
+        return result + 1;
+    }
+}
