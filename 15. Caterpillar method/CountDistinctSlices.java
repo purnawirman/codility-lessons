aa70f0fba29433ae7f09ea93df4ff927d9e88726
Purnawirman <purnawirman@gmail.com>
2021-04-05 18:35:24 +0700

    Create CountDistinctSlices.java

---
+++ 15. Caterpillar method/CountDistinctSlices.java
@@ -0,0 +1,23 @@
+// you can also use imports, for example:
+// import java.util.*;
+
+// you can write to stdout for debugging purposes, e.g.
+// System.out.println("this is a debug message");
+
+class Solution {
+    public int solution(int M, int[] A) {
+        // write your code in Java SE 8
+        int result = 0;
+        int[] counters = new int[M + 1];
+        int head = 0; 
+        for (int tail = 0; tail < A.length; tail++) {
+            while (counters[A[tail]] > 0) { // seen it before
+                counters[A[head]] = counters[A[head]] - 1;
+                head++;
+            }
+            result += tail - head + 1;
+            counters[A[tail]] = counters[A[tail]] + 1;
+        }
+        return result;
+    }
+}
