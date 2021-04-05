c537069e06916c326cfec112bcf2980802ae2547
Purnawirman <purnawirman@gmail.com>
2021-04-05 18:37:34 +0700

    Update CountDistinctSlices.java

--- 15. Caterpillar method/CountDistinctSlices.java
+++ 15. Caterpillar method/CountDistinctSlices.java
@@ -7,8 +7,9 @@
 class Solution {
     public int solution(int M, int[] A) {
         // write your code in Java SE 8
+        final int LIMIT = 1000000000;
         int result = 0;
-        int[] counters = new int[M + 1];
+        int[] counters = new int[M];
         int head = 0; 
         for (int tail = 0; tail < A.length; tail++) {
             while (counters[A[tail]] > 0) { // seen it before
@@ -16,6 +17,7 @@ class Solution {
                 head++;
             }
             result += tail - head + 1;
+            if (result > LIMIT) return LIMIT;
             counters[A[tail]] = counters[A[tail]] + 1;
         }
         return result;
